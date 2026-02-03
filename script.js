c4847acac177223bf17571113e0b17c1508f29e8
253079-Nakanishi <s253079f@st.u-kochi.ac.jp>
2026-02-03 12:50:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function showPopup(html) {
+  const overlay = document.getElementById("overlay");
+  const content = document.getElementById("popupContent");
 
+  content.innerHTML = html;
+  overlay.style.display = "flex";
+
+  setTimeout(()=> {
+    overlay.classList.add("show");
+  }, 50);
+}
+
+function closePopup() {
+  const overlay = document.getElementById("overlay");
+  overlay.classList.remove("show");
+
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 400);
+}
