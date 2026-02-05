323b9929ceda0287dea431aa50d7b951ea07320e
253079-Nakanishi <s253079f@st.u-kochi.ac.jp>
2026-02-06 01:46:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
 function showResult() {
-   alert("showResult 呼ばれた！");
+  const counts = { A: 0, B: 0, C: 0 };
+
+  for (let i = 1; i <= 4; i++) {
+    const checked = document.querySelector(
+      `input[name="q${i}"]:checked`
+    );
+
+    if (!checked) {
+      alert("まだ答えていない質問があるみたい");
+      return;
+    }
+
+    counts[checked.value]++;
+  }
+
+  let resultType = "A";
+  if (counts.B > counts[resultType]) resultType = "B";
+　if (counts.C > counts[resultType]) resultType = "C";
+
+  const results = {
+    A: {
+      title: "Frutiger Aero",
+      description: "好奇心旺盛で行動力あり！",
+      image: "https://i.redd.it/wthjhx8t5nvc1.png"
+    },
+    B: {
+      title: "Dream Core",
+      description: "落ち着き重視タイプ",
+      image: "https://www.the-sun.com/wp-content/uploads/sites/6/2023/05/comp-skc-dream-op.jpg"
+    },
+    C: {
+      title: "Cyber Core",
+      description: "自由で創造的タイプ",
+      image: "https://i.pinimg.com/originals/be/74/e8/be74e88d93805394c4f8d7636fad62e7.jpg"
+    }
+  };
+
   const result = results[resultType];
 showPopup(`
-  <h2>${result.title}</h2>
-  <p>${result.description}</p>
-  <img src="${result.image}" alt="">
-`);
+    <h2>${result.title}</h2>
+    <p>${result.description}</p>
+    <img src="${result.image}" alt="">
+  `);
 }
 
 function showPopup(html) {
-   alert("showPopup 入った！");
-  showPopup("ここまで来てる！");
+  alert("あなたにぴったりなのは…");
 
   const overlay = document.getElementById("overlay");
   const content = document.getElementById("popupContent");
-if (!overlay || !content) {
-    alert("overlay か popupContent が見つからない！");
-    return;
-  }
 
   content.innerHTML = html;
   overlay.style.display = "flex";
-
-setTimeout(() => {
-    overlay.classList.add("show");
-  }, 50);
 }
 
 function closePopup() {
-  const overlay = document.getElementById("overlay");
-  overlay.classList.remove("show");
-
-  setTimeout(() => {
-    overlay.style.display = "none";
-  }, 400);
+  document.getElementById("overlay").style.display = "none";
 }
